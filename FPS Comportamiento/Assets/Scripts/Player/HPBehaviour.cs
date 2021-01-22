ba3826ea8eb5b6a9e2dca51b5a5ff5b8ca832b6e
Pelirrojo <jorgesanworks@gmail.com>
2021-01-22 19:10:49 +0100

    sistema de vida del jugador

---
+++ FPS Comportamiento/Assets/Scripts/Player/HPBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBehaviour : MonoBehaviour
+{
+    //vida actual
+    public int currentHealth;
+    //vida maxima
+    public int maxHealth;
+    //objeto barra de vida
+    public GameObject healthBar;
+
+    private Scrollbar bar;
+
+    private void Start()
+    {
+        maxHealth = 100;
+        currentHealth = 100;
+        bar = healthBar.GetComponent<Scrollbar>();
+    }
+
+    public void getDamage(int cantidad)
+    {
+        if(maxHealth > 0)
+        {
+            currentHealth = currentHealth -10;
+            bar.size = currentHealth / maxHealth;
+            if (maxHealth <= 0)
+            {
+                die();
+            }
+        }
+        
+    }
+
+    public void getHealth(int cantidad)
+    {
+        if(currentHealth < maxHealth)
+        {
+            currentHealth = currentHealth + 10;
+            bar.size = currentHealth / maxHealth;
+        }
+    }
+
+    public void die()
+    {
+        Debug.Log("Tas muerto");
+    }
+    
+}
